67f9e3d4f801359633130e9108607c9d03867a8d
Russ Hatch <rhatch@datastax.com>
2015-01-21 12:11:05 -0700

    comments

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # It attempts to use the docker cache system as much as possible without building stale code
 # It creates git_sha.txt which is created to preserve or invalidate the docker cache when appropriate
 # Most importantly it creates the Dockerfile which is used to build the docker image
-# To make changes to the build steps, edit Dockerfile.template but be aware this will likely invalidate caches and make things take longer
+# To make changes to the build steps, edit Dockerfile.template -- this may invalidate caches and make things take longer temporarily
 if [ "$#" -eq 0 ]; then
     echo
     echo "usage: "
@@ -22,7 +22,7 @@ GIT_SHA=`git ls-remote -h -t https://github.com/apache/cassandra.git $GIT_SEARCH
 
 if [ -z "$GIT_SHA" ]; then
     echo "$GIT_SEARCH does not seem to be a branch or tag. Will attempt to build using value as commit id."
-    # we can't correlate the ref to a tag/branch, so lets set the SHA value to the user provided value which is now assumed to be a commitd$ SHA
+    # we can't correlate the ref to a tag/branch, so lets set the SHA value to the user provided value which is now assumed to be a commit
     GIT_SHA=$GIT_SEARCH
 fi
 
@@ -39,8 +39,8 @@ echo "# NOTE: this file is auto-generated! To make changes edit Dockerfile.templ
 # update Dockerfile to set git ref
 sed s/{{git_ref_to_build}}/$GIT_SEARCH/g Dockerfile.template >> Dockerfile
 
-# this just means the user provided git ref wasn't a tag or branch name
 if [ "$GIT_SEARCH" = "$GIT_SHA" ]; then
+    # apparently the command line argument was a raw commit id, we'll prefix the image name with 'commit' below
     GIT_SEARCH='commit'
 fi
 
